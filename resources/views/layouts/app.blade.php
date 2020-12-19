cb91ee98b98049dcc5c5db31b69fff1f271d5b28
baubrun <baubelf@gmail.com>
2020-12-19 11:35:09 -0500

    updated posts pagination

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <ul class="flex items-center">
             @auth
                 <li>
-                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
+                    <a href="" class="p-3"> Allo {{ auth()->user()->name }}!</a>
                 </li>
                 <li>
                     <form action="{{ route('logout') }}" method="POST" class="inline p-3">
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,6 +35,9 @@
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+
+                    {{ $posts->links() }}
+
             @else
                 <p>No posts.</p>
             @endif
